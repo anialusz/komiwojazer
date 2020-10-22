ee3c79f25671e208b95fad60e04dc811cc78d173
anial <anialuszczkiewicz@gmail.com>
2020-10-22 16:30:24 +0200

    code refactor

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import pygame.freetype
 import random
 import copy
 import math 
+import sys
 
 from City import City
 
@@ -10,16 +11,14 @@ shortestPath = []
 numberOfCities = 10
 screen_width = 1080
 screen_height = 720
-FPS = 60
 white = (255, 255, 255)
 yellow = (255, 255, 0)
 violet = (136, 78, 160)
 backgroundColor = (0, 0, 0)
 
 def main() :
-    cities = []
+    citiesList = []
     citiesOrder = []
-    newCitiesOrder = []
     programFinished = False
     run = True
     numberOfCheckedPaths = 0
@@ -29,7 +28,7 @@ def main() :
     screen = pygame.display.set_mode((screen_width, screen_height)) 
     font = pygame.freetype.Font("Montserrat-Regular.ttf", 24)
     
-    generateCitiesLocation(cities, citiesOrder) 
+    generateCitiesLocation(citiesList, citiesOrder) 
 
     while run:
         for event in pygame.event.get():
@@ -38,36 +37,36 @@ def main() :
         screen.fill(backgroundColor)
 
         if (programFinished == False) :
-            newCitiesOrder = applyNewCitesOrderToCitiesList(cities, citiesOrder)
+            citiesList = applyNewCitesOrderToCitiesList(citiesList, citiesOrder)
 
-            currentDistance = calculateDistanceBetweenAllCities(newCitiesOrder) 
-            shortestDistance = checkIfShortestPath(shortestDistance, currentDistance, newCitiesOrder)
+            currentDistance = calculateDistanceBetweenAllCities(citiesList) 
+            shortestDistance = checkIfShortestPath(shortestDistance, currentDistance, citiesList)
             numberOfCheckedPaths += 1
             font.render_to(screen, (10, 10), "Current shortest path length: " + str(round(shortestDistance, 2)), white)
             font.render_to(screen, (10, 50), "Progress of checked paths: " + str(round(((numberOfCheckedPaths/math.factorial(len(citiesOrder) - 1)) * 100), 2)) + "%", white)
 
-            drawCitiesLocations(pygame, screen, newCitiesOrder)
-            drawCurrentCheckOrder(pygame, screen, newCitiesOrder)
-            drawCurrentShortestPath(pygame, screen, shortestPath, newCitiesOrder)
+            drawCitiesLocations(pygame, screen, citiesList)
+            drawCurrentCheckOrder(pygame, screen, citiesList)
+            drawCurrentShortestPath(pygame, screen, shortestPath, citiesList)
 
             citiesOrder, programFinished = createNextPermutationOfCitiesList(citiesOrder, programFinished)
 
             pygame.display.update()
 
-def drawCitiesLocations(pygame, screen, newCitiesOrder) :
-    for n in range (1, len(newCitiesOrder)) :
-        pygame.draw.circle(screen, white, (newCitiesOrder[n].x, newCitiesOrder[n].y), 4)
-    pygame.draw.circle(screen, yellow, (newCitiesOrder[0].x, newCitiesOrder[0].y), 4)
+def drawCitiesLocations(pygame, screen, citiesList) :
+    for n in range (1, len(citiesList)) :
+        pygame.draw.circle(screen, white, (citiesList[n].x, citiesList[n].y), 4)
+    pygame.draw.circle(screen, yellow, (citiesList[0].x, citiesList[0].y), 4)
 
-def drawCurrentCheckOrder(pygame, screen, newCitiesOrder) :
-    for n in range (len(newCitiesOrder) - 1) :
-        pygame.draw.line(screen, white, (newCitiesOrder[n].x, newCitiesOrder[n].y), (newCitiesOrder[n + 1].x, newCitiesOrder[n + 1].y))
-    pygame.draw.line(screen, white, (newCitiesOrder[0].x, newCitiesOrder[0].y), (newCitiesOrder[len(newCitiesOrder) - 1].x, newCitiesOrder[len(newCitiesOrder) - 1].y))
+def drawCurrentCheckOrder(pygame, screen, citiesList) :
+    for n in range (len(citiesList) - 1) :
+        pygame.draw.line(screen, white, (citiesList[n].x, citiesList[n].y), (citiesList[n + 1].x, citiesList[n + 1].y))
+    pygame.draw.line(screen, white, (citiesList[0].x, citiesList[0].y), (citiesList[len(citiesList) - 1].x, citiesList[len(citiesList) - 1].y))
 
-def drawCurrentShortestPath(pygame, screen, shortestPath, newCitiesOrder) :
+def drawCurrentShortestPath(pygame, screen, shortestPath, citiesList) :
     for n in range (len(shortestPath) - 1) :
         pygame.draw.line(screen, violet, (shortestPath[n].x, shortestPath[n].y), (shortestPath[n + 1].x, shortestPath[n + 1].y), 3)
-    pygame.draw.line(screen, violet, (shortestPath[0].x, shortestPath[0].y), (shortestPath[len(newCitiesOrder) -1].x, shortestPath[len(newCitiesOrder) - 1].y), 3)
+    pygame.draw.line(screen, violet, (shortestPath[0].x, shortestPath[0].y), (shortestPath[len(citiesList) -1].x, shortestPath[len(citiesList) - 1].y), 3)
 
 def createNextPermutationOfCitiesList(citiesOrder, programFinished) :
     largestX = -1
@@ -92,17 +91,18 @@ def createNextPermutationOfCitiesList(citiesOrder, programFinished) :
 
     return citiesOrder, programFinished
 
-def applyNewCitesOrderToCitiesList(cities, citiesOrder) :
-    newCityListOrder = []
+def applyNewCitesOrderToCitiesList(citiesList, citiesOrder) :
+    newCityList = []
     for n in citiesOrder :
-        newCityListOrder.append(cities[n])
+        newCityList.append(citiesList[n])
     
-    return newCityListOrder
+    return newCityList
 
 def generateCitiesLocation(citiesList, citiesOrder) :
+    screenOffset = 70
     for cityNumber in range (numberOfCities) :
-        x = random.randint(70, screen_width - 70)
-        y = random.randint(70, screen_height - 70)
+        x = random.randint(screenOffset, screen_width - screenOffset)
+        y = random.randint(screenOffset, screen_height - screenOffset)
         createCity(citiesList, x, y)
         citiesOrder.append(cityNumber)
 
@@ -119,11 +119,10 @@ def calculateDistanceBetweenAllCities(cities_list) :
     totalDistance += returnRoute
     return totalDistance
 
-
-def checkIfShortestPath(savedShortestPath, currentPath, newCitiesOrder) :
+def checkIfShortestPath(savedShortestPath, currentPath, citiesList) :
     if (currentPath < savedShortestPath) :
         global shortestPath 
-        shortestPath = copy.deepcopy(newCitiesOrder)
+        shortestPath = copy.deepcopy(citiesList)
         return currentPath
     else:
         return savedShortestPath
